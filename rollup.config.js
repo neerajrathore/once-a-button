114c55914234a9735fe0ce6c3e652e4ef74177a1
Neeraj Rathore <neeraj8x@outlook.com>
2024-08-02 16:02:02 +0530

    init rollup and its config file

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+// this file export an array of configuration
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+
+// commonjs module used require syntax
+const packageJson = require('./package.json')
+
+// first main gonna export javascript files that has all our component in it
+// second module exporting the types the dot ts files that describe the types of component
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
