deb2cf9bee9e7fdefad22883707c305e9dbf2eae
Neeraj Rathore <neeraj8x@outlook.com>
2024-08-08 16:50:34 +0530

    extra properties removed and verified dist

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,73 +1,41 @@
 import { readFileSync } from 'fs';
-import { defineConfig } from 'rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
 import dts from 'rollup-plugin-dts';
-import typescriptEngine from 'typescript';
+// import * as packageJson from './package.json' assert { type: "json" };
 
+// const packageJson = require('./package.json');
 const packageJson = JSON.parse(readFileSync('./package.json'));
 
-export default defineConfig(
+
+export default [
   {
+    // entry point for our library
     input: './src/index.ts',
     output: [
       {
         file: packageJson.main,
         format: 'cjs',
-        sourcemap: false,
-        exports: 'named',
-        name: packageJson.name,
+        sourcemap: false
       },
       {
         file: packageJson.module,
         format: 'es',
-        exports: 'named',
-        sourcemap: false,
+        sourcemap: false
       },
     ],
     plugins: [
-      postcss({
-        plugins: [],
-        minimize: true,
-      }),
-      external({ includeDependencies: true }),
       resolve(),
       commonjs(),
-      typescript({
-        tsconfig: './tsconfig.json',
-        typescript: typescriptEngine,
-        sourceMap: false,
-        exclude: [
-          'coverage',
-          '.storybook',
-          'storybook-static',
-          'config',
-          'dist',
-          'node_modules/**',
-          '*.cjs',
-          '*.mjs',
-          '**/__snapshots__/*',
-          '**/__tests__',
-          '**/*.test.js+(|x)',
-          '**/*.test.ts+(|x)',
-          '**/*.mdx',
-          '**/*.story.ts+(|x)',
-          '**/*.story.js+(|x)',
-          '**/*.stories.ts+(|x)',
-          '**/*.stories.js+(|x)',
-          'setupTests.ts',
-          'vitest.config.ts',
-        ],
+      typescript({ 
+        tsconfig: './tsconfig.json'
       })
     ],
   },
   {
-    input: 'dist/esm/types/src/index.d.ts',
+    input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    external: [/\.(sc|sa|c)ss$/],
-    plugins: [dts()],
+    plugins: [dts()]
   },
-);
+];
