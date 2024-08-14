03c75dc0508330e8ab3894f434cff0d1f4c9e419
Neeraj Rathore <neeraj8x@outlook.com>
2024-08-14 15:11:19 +0530

    storybook added for testing component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import './Button.css'
 
-interface ButtonProps {
-    label: string
+export interface ButtonProps {
+    primary?: boolean;
+    label: string;
+    size?: 'small' | 'medium' | 'large';
+    backgroundColor?: string;
+    onClick: () => void;
 }
 
 // webpack is create bundle single file for large application 
 // roll up is used to bundle libraries to create packages
-const Button = (props: ButtonProps) => {
+const Button = ({ primary = false, label, size = 'medium', backgroundColor, ...props }: ButtonProps) => {
+    const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
     return (
-        <button>{props.label}</button>
+        <button
+            // className={`storybook-button storybook-button--${size}`} 
+            className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+            style={{ backgroundColor }}
+            {...props}
+        >{label}
+        </button>
     )
 }
 
